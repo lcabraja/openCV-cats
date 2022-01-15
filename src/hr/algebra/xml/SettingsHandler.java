e88558a44b3ebaf6d45d7d179b3af6f93b8ec33b
Luka Cabraja <luka@cabraja.eu>
2022-01-15 01:14:23 +0100

    additional error handling

--- src/hr/algebra/xml/SettingsHandler.java
+++ src/hr/algebra/xml/SettingsHandler.java
@@ -8,6 +8,7 @@ package hr.algebra.xml;
 import hr.algebra.OpenCVCats;
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -34,10 +35,23 @@ public class SettingsHandler {
     }
 
     private static void serializeToXML(Settings settings) {
-        try (FileOutputStream fos = new FileOutputStream("settings.xml"); XMLEncoder encoder = new XMLEncoder(fos)) {
-            encoder.writeObject(settings);
+        try {
+            createSettingsFile();
+            try (FileOutputStream fos = new FileOutputStream("settings.xml"); XMLEncoder encoder = new XMLEncoder(fos)) {
+                encoder.writeObject(settings);
+            }
         } catch (IOException ex) {
-            Logger.getLogger(SettingsHandler.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Cannot access settings file...");
+            createSettingsFile();
+        }
+    }
+
+    private static void createSettingsFile() {
+        try {
+            File settingsFile = new File("settings.xml");
+            settingsFile.createNewFile();
+        } catch (IOException ex) {
+            System.err.println("Error while creating settings file");
         }
     }
 
@@ -46,7 +60,8 @@ public class SettingsHandler {
         try (FileInputStream fis = new FileInputStream("settings.xml"); XMLDecoder decoder = new XMLDecoder(fis)) {
             decodedSettings = (Settings) decoder.readObject();
         } catch (IOException ex) {
-            Logger.getLogger(SettingsHandler.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Cannot access settings file...");
+            createSettingsFile();
         }
         return decodedSettings;
     }
